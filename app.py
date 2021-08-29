c47aac12c4b32a212f8aa66537708b46f43c9f70
Henry Semakula <52510807+dbms-sema@users.noreply.github.com>
2021-08-29 10:22:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,30 +24,30 @@ db = SQLAlchemy(app)
 class Details(db.Model):
     __tablename__ = 'details'
     id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Integer)
     sex = db.Column(db.String(20))
     residence = db.Column(db.String(20))
+    bmi = db.Column(db.Float)
     alcohol = db.Column(db.String(20))
     smoker = db.Column(db.String(20))
-    age = db.Column(db.Integer)
     sbp = db.Column(db.Float)
-    dbp = db.Column(db.Float)
-    bmi = db.Column(db.Float)
     fhd = db.Column(db.String(20))
     hypertension = db.Column(db.String(20))
+    dbp = db.Column(db.Float)
     obesity = db.Column(db.String(20))
     physically_inactive = db.Column(db.String(20))
 
-    def __init__(self,sex,residence,alcohol,smoker,age,sbp,dbp,bmi,fhd,hypertension,obesity,physically_inactive):
+    def __init__(self,age,sex,residence,bmi,alcohol,smoker,sbp,fhd,hypertension,dbp,obesity,physically_inactive):
+        self.age = age
         self.sex = sex
         self.residence = residence
+        self.bmi = bmi
         self.alcohol = alcohol
         self.smoker = smoker
-        self.age = age
         self.sbp = sbp
-        self.dbp = dbp
-        self.bmi = bmi
         self.fhd = fhd
         self.hypertension = hypertension
+        self.dbp = dbp
         self.obesity = obesity
         self.physically_inactive = physically_inactive
 
@@ -60,25 +60,25 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     if request.method == 'POST':
+        age = request.form['age']
         sex = request.form['sex']
         residence = request.form['residence']
+        bmi = request.form['bmi']
         alcohol = request.form['alcohol']
         smoker= request.form['smoker']
-        age = request.form['age']
         sbp = request.form['sbp']
-        dbp = request.form['dbp']
-        bmi = request.form['bmi']
         fhd = request.form['fhd']
         hypertension = request.form['hypertension']
+        dbp = request.form['dbp']
         obesity = request.form['obesity']
         physically_inactive = request.form['physically_inactive']
-        if sex =='' or residence == '' or alcohol=='' or smoker=='' or age == '' or sbp == '' or dbp == '' or bmi =='' or  fhd =='' or hypertension=='' or obesity =='' or physically_inactive =='':
+        if age == '' or sex =='' or residence == '' or bmi =='' or alcohol=='' or smoker=='' or sbp == '' or  fhd =='' or hypertension=='' or dbp == '' or obesity =='' or physically_inactive =='':
             return render_template('index.html', message='<b>Please All Fields Must Be Filled In !!</b>')
         else:
-            data = Details(sex,residence,alcohol,smoker,age,sbp,dbp,bmi,fhd,hypertension,obesity,physically_inactive)
+            data = Details(age,sex,residence,bmi,alcohol,smoker,sbp,fhd,hypertension,dbp,obesity,physically_inactive)
             db.session.add(data)
             db.session.commit()
-            data = np.array([[sex,residence,alcohol,smoker,age,sbp,dbp,bmi,fhd,hypertension,obesity,physically_inactive]])
+            data = np.array([[age,sex,residence,bmi,alcohol,smoker,sbp,fhd,hypertension,dbp,obesity,physically_inactive]])
             my_prediction = classifier.predict(data)
             return render_template('result.html', prediction=my_prediction)
 
