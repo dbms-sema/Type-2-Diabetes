86a27616e0d18a98a38e48b430b23a0bd1c85cc9
Henry Semakula <52510807+dbms-sema@users.noreply.github.com>
2021-08-31 12:10:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.debug = True
 app.config['DATABASE_URI'] = 'postgres://mvkwzjbntalnko:365226844a4af9cc89bff9a45e6bf57ae3f91d5ceb9dca919bb608628cf706e2@ec2-52-203-74-38.compute-1.amazonaws.com:5432/d5b2hjlk5kkf9'
 
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Fasle
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 class Details(db.Model):
